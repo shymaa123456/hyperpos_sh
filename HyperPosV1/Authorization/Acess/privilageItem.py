590cc69578e9e81e9fbd6c7f8e9c00771fe271db
shaymaa <shymaa.helal@hyperone.com.eg>
2020-08-17 10:54:02 +0200

    Fawzy changes on 17.8.2020

--- HyperPosV1/Authorization/Acess/privilageItem.py
+++ HyperPosV1/Authorization/Acess/privilageItem.py
@@ -14,28 +14,32 @@ class CL_privilageItem(QtWidgets.QDialog):
     def __init__(self):
         super(CL_privilageItem, self).__init__()
 
-        #7
+        #
 
-    def showMessageBox(self, title, message):
-        msgbox = QtWidgets.QMessageBox()
-        msgbox.setIcon( QtWidgets.QMessageBox.Warning )
-        msgbox.setWindowTitle( title )
-        msgbox.setText( message )
-        msgbox.setStandardButtons( QtWidgets.QMessageBox.Ok )
-        msgbox.exec_()
 
     def FN_LOAD_CREATE(self):
         loadUi('../Presentation/createPrivilageItem.ui', self)
         self.BTN_createPrivItem.clicked.connect(self.FN_CREATE_PRIVILAGE_ITEM)
         self.FN_GET_PRIV()
+        self.FN_GET_ITEMS()
         self.FN_GET_FORMS()
-        self.CMB_priv.currentIndexChanged.connect(self.FN_GET_FORMS)
-        self.CMB_form.currentIndexChanged.connect(self.FN_GET_FORMID)
-        self.CMB_formItem.currentIndexChanged.connect(self.FN_GET_ITEMID)
-        self.CMB_status.addItems(["0", "1"])
+
+
         self.FN_GET_FORMID()
         self.FN_GET_ITEMID()
 
+
+        self.CMB_form.currentIndexChanged.connect(self.FN_GET_FORMID)
+        self.CMB_formItem.currentIndexChanged.connect(self.FN_GET_ITEMID)
+        self.CMB_status.addItems(["0", "1"])
+    # def FN_LOAD_MODFIY(self):
+    #     loadUi('../Presentation/modifyPrivilage.ui', self)
+    #     self.BTN_getPrivilage.clicked.connect(self.FN_GET_PRIV)
+    #     self.BTN_modifyPrivilage.clicked.connect(self.FN_MODIFY_PRIV)
+    #     self.CMB_roleId.currentIndexChanged.connect(self.FN_GET_ROLENAME)
+    #     self.CMB_formId.currentIndexChanged.connect(self.FN_GET_FORMNAME)
+    #     self.CMB_actionId.currentIndexChanged.connect(self.FN_GET_ACTIONNAME)
+    #
     def FN_GET_PRIV(self):
         connection = mysql.connector.connect(host='localhost', database='PosDB'
                                              , user='root', password='password', port='3306')
@@ -45,43 +49,37 @@ class CL_privilageItem(QtWidgets.QDialog):
         records = mycursor.fetchall()
         for row in records:
             self.CMB_priv.addItems([row[0]])
+
+
         connection.close()
         mycursor.close()
 
     def FN_GET_FORMS(self):
-        self.CMB_form.clear()
-        self.priv=self.CMB_priv.currentText()
         connection = mysql.connector.connect(host='localhost', database='PosDB'
                                              , user='root', password='password', port='3306')
         mycursor = connection.cursor()
-        sql = "SELECT FORM_DESC FROM SYS_FORM as f INNER join SYS_PRIVILEGE  as p on f.FORM_ID = p.FORM_ID WHERE p.PRIV_ID = "+self.priv
-        #print(sql)
-        mycursor.execute(sql)
+
+        mycursor.execute("SELECT FORM_DESC FROM SYS_FORM order by FORM_ID asc")
         records = mycursor.fetchall()
-        if mycursor.rowcount > 0:
-            for row in records:
-                self.CMB_form.addItems([row[0]])
+        for row in records:
+            self.CMB_form.addItems([row[0]])
 
 
         connection.close()
         mycursor.close()
-        self.FN_GET_ITEMS()
 
     def FN_GET_ITEMS(self):
-        self.CMB_formItem.clear()
         connection = mysql.connector.connect(host='localhost', database='PosDB'
                                              , user='root', password='password', port='3306')
         mycursor = connection.cursor()
         self.form = self.CMB_form.currentText()
-        sql_select_query = "select ITEM_DESC , ITEM_STATUS from SYS_FORM_ITEM as i inner join SYS_FORM as f on " \
-                           "i.FORM_ID = f.FORM_ID " \
-                           "where FORM_DESC = '" + self.form +"'"
-        #print(sql_select_query)
+
+        sql_select_query = "select ITEM_DESC , ITEM_STATUS from SYS_FORM_ITEM where FORM_ID = '" + self.form + "'"
+
         mycursor.execute(sql_select_query)
         records = mycursor.fetchall()
-        if mycursor.rowcount > 0:
-            for row in records:
-                self.CMB_formItem.addItems([row[0]])
+        for row in records:
+            self.CMB_formItem.addItems([row[0]])
 
         connection.close()
         mycursor.close()
@@ -91,13 +89,12 @@ class CL_privilageItem(QtWidgets.QDialog):
         connection = mysql.connector.connect(host='localhost', database='PosDB'
                                              , user='root', password='password', port='3306')
         mycursor = connection.cursor()
-        sql_select_query = "SELECT FORM_ID FROM SYS_FORM WHERE FORM_DESC = '"+self.form+"'"
-
-        mycursor.execute(sql_select_query)
+        sql_select_query = "SELECT FORM_ID FROM SYS_FORM WHERE FORM_DESC = %s"
+        x = (self.form,)
+        mycursor.execute(sql_select_query, x)
 
         myresult = mycursor.fetchone()
-        if mycursor.rowcount > 0:
-            self.LB_formId.setText(myresult[0])
+        self.LB_formId.setText(myresult[0])
 
         connection.close()
         mycursor.close()
@@ -107,13 +104,12 @@ class CL_privilageItem(QtWidgets.QDialog):
         connection = mysql.connector.connect(host='localhost', database='PosDB'
                                              , user='root', password='password', port='3306')
         mycursor = connection.cursor()
-        sql_select_query = "SELECT ITEM_ID FROM SYS_FORM_ITEM WHERE ITEM_DESC = '"+ self.item+"'"
-
-        mycursor.execute(sql_select_query)
+        sql_select_query = "SELECT ITEM_ID FROM SYS_FORM_ITEM WHERE ITEM_DESC = %s"
+        x = (self.form,)
+        mycursor.execute(sql_select_query, x)
 
         myresult = mycursor.fetchone()
-        if mycursor.rowcount > 0:
-            self.LB_formItemId.setText(myresult[0])
+       # self.LB_formItemId.setText(myresult[0])
 
         connection.close()
         mycursor.close()
@@ -123,31 +119,34 @@ class CL_privilageItem(QtWidgets.QDialog):
     def FN_CREATE_PRIVILAGE_ITEM(self):
         #self.name = self.LE_name.text()
         #self.desc= self.LE_DESC.text()
-        self.priv = self.CMB_priv.currentText()
-        self.form = self.LB_formId.text()
-        self.formItem = self.LB_formItemId.text()
-        self.status = self.CMB_status.currentText()
-        try:
-            connection = mysql.connector.connect(host='localhost', database='PosDB'
+
+        self.role = self.CMB_roleId.currentText()
+        self.form = self.CMB_formId.currentText()
+        self.action = self.CMB_actionId.currentText()
+
+        connection = mysql.connector.connect(host='localhost', database='PosDB'
                                              , user='root', password='password', port='3306')
-            mycursor = connection.cursor()
+        mycursor = connection.cursor()
+        # get max userid
+        mycursor.execute("SELECT max(PRIV_ID) FROM SYS_PRIVILEGE")
+        myresult = mycursor.fetchone()
 
-            sql = "INSERT INTO SYS_PRIVILEG_ITEM (PRIV_ID, FORM_ID,ITEM_ID,ITEM_STATUS)         " \
-                  "VALUES ( %s, %s, %s, %s)"
-
-            val = (self.priv, self.form, self.formItem, self.status)
-            mycursor.execute( sql, val )
-            connection.commit()
-
-            print( mycursor.rowcount, "record inserted." )
-            connection.close()
-            mycursor.close()
-            self.close()
-        except mysql.connector.Error as err:
-            print( "Something went wrong: {}".format( err ) )
-            self.showMessageBox( "Error", "Duplicate Entry" )
-            connection.close()
-            mycursor.close()
+        if myresult[0] == None:
+            self.id = "1"
+        else:
+            self.id = int(myresult[0]) + 1
 
 
+        sql = "INSERT INTO SYS_PRIVILEGE (PRIV_ID, ROLE_ID,FORM_ID,ACTION_ID)         " \
+              "VALUES ( %s, %s, %s, %s)"
 
+        val = (self.id, self.role, self.form,   self.action)
+        mycursor.execute(sql, val)
+        connection.commit()
+
+        connection.close()
+        mycursor.close()
+
+        print(mycursor.rowcount, "record inserted.")
+
+        self.close()
